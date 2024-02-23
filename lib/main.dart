40b7599607b89af92464bf234030c9db5758248a
noureen <noureensaad086@gmail.com>
2024-02-23 20:03:24 +0200

    Basics of the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/layout/login/login_screen.dart';
+import 'package:todo_app/style/app_theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +12,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ToDo',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      routes: {
+        LoginScreen.route:(_)=>LoginScreen(),
+      },
+      initialRoute: LoginScreen.route,
     );
   }
 }
